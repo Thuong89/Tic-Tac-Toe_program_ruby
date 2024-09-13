4e69a01daa55b3af75979ba971c3617f255f8cdd
Thuong <Thuong89@users.noreply.github.com>
2024-09-13 11:51:21 +0300

    add logic for checkifwinner.rb

--- tictactoe/libs/checkifwinner.rb
+++ tictactoe/libs/checkifwinner.rb
@@ -10,9 +10,15 @@ class Winner
         return 'o won'
       end
     end
+
+    d1= (board[0][0]).to_s + (board[1][1]).to_s + (board[2][2]).to_s
+    d2= (board[2][0]).to_s + (board[1][1]).to_s + (board[0][2]).to_s
+    if d1=='xxx' or d2='xxx'
+      return 'x won'
+    end
+    if d2=='ooo' or d2='ooo'
+      return 'o won'
+    end
   
   end
 end
-
-f=Winner.new
-print f.checking([['x','x','x'],[1,2,3],[4,5,6]])
